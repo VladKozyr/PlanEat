4c96abcb51977b7b912993dcb3837a3e2a0dde76
Vlad Kozyr <vladyslav_kozyr@epam.com>
2021-05-26 00:00:28 +0300

    Add strapi server

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ec2494a7025c110164bc288c27a21745'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '923ad73a-3ebb-414e-83cf-8aa29712e650'
+};
